034576de67de62e74b8d88d386374d6ba81ba730
Alvarenga7 <kiteyyhastenreiter@gmail.com>
2025-02-07 11:57:42 -0300

    feat:criacao do componente Tag.tsx

---
+++ src/components/Tag.tsx
@@ -0,0 +1,16 @@
+import { HTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
+
+export default function Tag(props: HTMLAttributes<HTMLDivElement>) {
+    const { className, children, ...otherProps } = props;
+
+    return (
+        <div className={twMerge("", className)} {...otherProps}>
+
+            <span>&#10038;</span>
+            <span>{children}</span>
+
+        </div>
+
+    )
+}
