764e5cc10c4ee24fcc519c1ad570ad00b5907cb4
Alvarenga7 <kiteyyhastenreiter@gmail.com>
2025-02-07 12:13:22 -0300

    feat:ativando a Feature.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/sections/Navbar"
 import Hero from "@/sections/Hero";
 import LogoTicker from "@/sections/LogoTicker";
 import Interaction from "@/sections/Interaction";
+import Feature from "@/sections/Features";
 
 export default function Home() {
     return (
@@ -10,6 +11,7 @@ export default function Home() {
             <Hero />
             <LogoTicker />
             <Interaction />
+            <Features />
         </>
     );
 }
