9d8298a77a0cfb24ea054963e820192bd94526f8
Alvarenga7 <kiteyyhastenreiter@gmail.com>
2025-02-07 15:00:29 -0300

    feat:ajustando o Avatar.tsx

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,6 +1,14 @@
-export default function Avatar(){
+import { HTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
+
+export default function Avatar(props: HTMLAttributes<HTMLDivElement>){
+
+    const { className, children, ...otherProps } = props;    
+
 
     return(
-        
+            <div className={twMerge("", className)} {...otherProps}>
+                {children}
+                </div>
     )
 }
